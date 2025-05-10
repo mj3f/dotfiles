0cbf3c46567dbaf7457b813e88ab4e4075c6e7be
Mike Pratt <mike@msjp.me>
2025-05-10 12:33:41 +0100

    Fix: Added workaround that fixed issue with mason plugin

---
+++ nvim/lua/plugins/mason-fix.lua
@@ -0,0 +1,5 @@
+# This fixes the bug with mason as described in this github issue: https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2867910401
+return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
